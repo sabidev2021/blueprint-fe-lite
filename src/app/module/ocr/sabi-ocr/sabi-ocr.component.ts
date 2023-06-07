7e925439c025205bf254360960db878763ab748d
Dimas Prasetyo <dimas@sahabatbisnis.co.id>
2023-06-07 12:04:58 +0700

    remove unused variable decralation

--- src/app/module/ocr/sabi-ocr/sabi-ocr.component.ts
+++ src/app/module/ocr/sabi-ocr/sabi-ocr.component.ts
@@ -31,19 +31,6 @@ export class SabiOcrComponent implements OnInit {
     isLoading: boolean = false;
     isSubmited: boolean = false;
     isValidKtp: boolean = true;
-    birth_place!: string;
-    birth_date!: string;
-    gender!: string;
-    rt!: string;
-    rw!: string;
-    village!: string;
-    subdistrict!: string;
-    religion!: string;
-    martial_status!: string;
-    work!: string;
-    citizenship!: string;
-    valid_until!: string;
-    blood_type!: string;
     list: Array<any> = [];
     public configStage = new BehaviorSubject({
         width: 200,
